332073d300d8ec25049384e7a9b97a1b41cf6409
Oleksii Uzhva <garom.gift@gmail.com>
2023-11-21 12:44:26 +0200

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_button_clickMe_clicked()
+{
+    ui->label_greeting->setText("Hello world!");
+}
+
